89250d56dd47a695ccedd73e41c0f359614661df
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-08 12:07:08 +0100

    📄 First commit.

---
+++ src/main/java/net/slqmy/first_plugin/FirstPlugin.java
@@ -0,0 +1,17 @@
+package net.slqmy.first_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
