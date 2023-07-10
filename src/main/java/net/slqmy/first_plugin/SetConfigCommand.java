a974b5e62a68a19fcfda0b635d04f280428864bb
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-10 01:41:35 +0100

    📄 Added /set-config.

---
+++ src/main/java/net/slqmy/first_plugin/SetConfigCommand.java
@@ -0,0 +1,38 @@
+package net.slqmy.first_plugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class SetConfigCommand implements CommandExecutor {
+	FirstPlugin firstPlugin;
+
+	public SetConfigCommand(FirstPlugin firstPlugin) {
+		this.firstPlugin = firstPlugin;
+	}
+
+	@Override
+	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, String[] arguments) {
+		if (commandSender instanceof Player) {
+			Player player = (Player)commandSender;
+
+			if (arguments.length != 2) {
+				player.sendMessage("You must provide 2 arguments!");
+			} else {
+				firstPlugin.getConfig().set("Word", arguments[0]);
+				List<String> stringList = firstPlugin.getConfig().getStringList("Fruits");
+				stringList.add(arguments[1]);
+
+				firstPlugin.getConfig().set("Fruits", stringList);
+
+				firstPlugin.saveConfig();
+			}
+		}
+
+		return false;
+	}
+}
