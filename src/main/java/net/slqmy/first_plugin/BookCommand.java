11ad52d576d33040c83d9fcc70f4204539b7055d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-15 20:51:26 +0100

    📗 Added clickable and hoverable text to the book.

--- src/main/java/net/slqmy/first_plugin/BookCommand.java
+++ src/main/java/net/slqmy/first_plugin/BookCommand.java
@@ -1,5 +1,10 @@
 package net.slqmy.first_plugin;
 
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -20,19 +25,30 @@ public class BookCommand implements CommandExecutor {
 			ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
 
 			BookMeta bookMeta = (BookMeta) book.getItemMeta();
+			assert bookMeta != null;
 
 			bookMeta.setTitle(ChatColor.RED + "The entire history of The Slimy Swamp, I guess");
 			bookMeta.setAuthor(ChatColor.GREEN + "Slqmy");
 
 		  bookMeta.addPage(
-							ChatColor.GREEN + "The Slimy Swamp was founded by Slqmy."
-							+ ChatColor.WHITE + "\n\nIt was a really cool server."
-		          + "It had many players."
-		          + "But eventually it died."
-		          + "But it will be revived! Hopefully."
+							ChatColor.GREEN + "The Slimy Swamp was founded by Slqmy.\n\n"
+							+ ChatColor.WHITE + "It was a really cool server.\n"
+		          + "It had many players.\n"
+		          + "But eventually it died.\n"
+		          + "But it will be revived! Hopefully.\n"
 		  );
 
-			bookMeta.setGeneration(BookMeta.Generation.TATTERED);
+			TextComponent clickable = new TextComponent(ChatColor.YELLOW.toString() + ChatColor.BOLD + "Click me!\n\n");
+			clickable.setClickEvent(new ClickEvent(ClickEvent.Action.CHANGE_PAGE, "1"));
+
+			TextComponent none = new TextComponent(ChatColor.GRAY + "This does nothing.\n");
+
+			TextComponent hoverable = new TextComponent(ChatColor.AQUA + "Hover over me to see the secret message!");
+			hoverable.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Troll!")));
+
+			bookMeta.spigot().addPage(new BaseComponent[]{ clickable, none, hoverable });
+
+			bookMeta.setGeneration(BookMeta.Generation.ORIGINAL);
 
 			book.setItemMeta(bookMeta);
 
