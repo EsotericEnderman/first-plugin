5c33efe39abad18731e4d4fff4f55bbc07ac0a6b
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:06:54 +0100

    🔼 Improved code and messages.

--- src/main/java/net/slqmy/first_plugin/commands/BuffCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/BuffCommand.java
@@ -13,28 +13,50 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
+import net.slqmy.first_plugin.utility.Utility;
+
 import java.util.UUID;
 
-public class BuffCommand implements CommandExecutor {
+public final class BuffCommand implements CommandExecutor {
+	private static final int GENERIC_ATTACK_DAMAGE_INCREASE = 10;
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
-
-			ItemStack playerHeldItem = player.getInventory().getItemInMainHand();
-
-			if (playerHeldItem != null && playerHeldItem.getType() != Material.AIR) {
-				AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(), Attribute.GENERIC_ATTACK_DAMAGE.toString(), 10, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND);
-
-				ItemMeta mainHandMeta = playerHeldItem.getItemMeta();
-				mainHandMeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
-				playerHeldItem.setItemMeta(mainHandMeta);
-
-				player.sendMessage(ChatColor.MAGIC + "Item buffed!");
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 0) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			final ItemStack playerHeldItem = player.getInventory().getItemInMainHand();
+
+			if (playerHeldItem.getType() == Material.AIR) {
+				player.sendMessage(ChatColor.RED + "You must be holding an item to run this command!");
+
+				return true;
+			}
+
+			final AttributeModifier modifier = new AttributeModifier(UUID.randomUUID(),
+					Attribute.GENERIC_ATTACK_DAMAGE.toString(), GENERIC_ATTACK_DAMAGE_INCREASE,
+					AttributeModifier.Operation.ADD_NUMBER,
+					EquipmentSlot.HAND);
+
+			final ItemMeta mainHandMeta = playerHeldItem.getItemMeta();
+
+			mainHandMeta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, modifier);
+
+			playerHeldItem.setItemMeta(mainHandMeta);
+
+			player.sendMessage(ChatColor.LIGHT_PURPLE + "Item buffed! " + ChatColor.AQUA + "Have fun!");
+		} else {
+			Utility.log("/buff is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
