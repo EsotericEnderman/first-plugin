e1bb72f85da33c4e5900ae76616196a429a4c75d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:08:34 +0100

    📄 Improved code and messages.

--- src/main/java/net/slqmy/first_plugin/commands/CooldownCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/CooldownCommand.java
@@ -2,6 +2,9 @@ package net.slqmy.first_plugin.commands;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+
+import net.slqmy.first_plugin.utility.Utility;
+
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -10,28 +13,48 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
-public class CooldownCommand implements CommandExecutor {
+public final class CooldownCommand implements CommandExecutor {
+	private static final int COOLDOWN_SECONDS = 5;
 
-	private Cache<UUID, Long> cooldown = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.SECONDS).build();
+	private final Cache<UUID, Long> cooldown = CacheBuilder.newBuilder()
+			.expireAfterWrite(COOLDOWN_SECONDS, TimeUnit.SECONDS)
+			.build();
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-
-			if (!cooldown.asMap().containsKey(player.getUniqueId())) {
-				player.sendMessage(ChatColor.GREEN + "Command successful! Cool-down set to 5 seconds");
-
-				cooldown.put(player.getUniqueId(), System.currentTimeMillis() + 5000);
-			} else {
-				long timeDifferenceMilliseconds = cooldown.asMap().get(player.getUniqueId()) - System.currentTimeMillis();
-
-				player.sendMessage(ChatColor.RED + "You must wait " + (timeDifferenceMilliseconds / 1000) + " more seconds until you can run this command again!");
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 0) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+			final UUID playerUUID = player.getUniqueId();
+			final ConcurrentMap<UUID, Long> map = cooldown.asMap();
+
+			if (!map.containsKey(playerUUID)) {
+				player.sendMessage(ChatColor.GREEN + "Command successful! Cool-down set to " + ChatColor.BOLD + COOLDOWN_SECONDS
+						+ ChatColor.GREEN + " seconds");
+
+				cooldown.put(playerUUID, System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(COOLDOWN_SECONDS));
+			} else {
+				final long timeDifferenceMilliseconds = map.get(playerUUID) - System.currentTimeMillis();
+
+				player
+						.sendMessage(ChatColor.RED + "You must wait " + ChatColor.BOLD
+								+ TimeUnit.MILLISECONDS.toSeconds(timeDifferenceMilliseconds)
+								+ ChatColor.RED + " more seconds until you can run this command again!");
+			}
+		} else {
+			Utility.log("/cool-down is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
