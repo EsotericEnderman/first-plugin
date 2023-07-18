b1e9068d15dc3768dfc5835455307c17b2c0fe64
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:07:51 +0100

    💻 Improved code and changed command to use the `Pair` utility class.

--- src/main/java/net/slqmy/first_plugin/commands/ComplexGamingMenuCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/ComplexGamingMenuCommand.java
@@ -1,7 +1,10 @@
 package net.slqmy.first_plugin.commands;
 
 import com.google.common.collect.Multimap;
-import net.slqmy.first_plugin.Utility;
+
+import net.slqmy.first_plugin.utility.Utility;
+import net.slqmy.first_plugin.utility.types.Pair;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
@@ -16,25 +19,62 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 
-public class ComplexGamingMenuCommand implements CommandExecutor {
+public final class ComplexGamingMenuCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
-
-			ItemStack duels = Utility.createItem(Material.DIAMOND_SWORD, ChatColor.AQUA + "Duels", (Multimap<Attribute, AttributeModifier>) null);
-			ItemStack frame = Utility.createItem(Material.LIGHT_GRAY_STAINED_GLASS_PANE, ChatColor.GRAY + "Complex Gaming");
-			ItemStack duelStatus = Utility.createItem(Material.BARRIER, ChatColor.RED.toString() + ChatColor.BOLD + "✘" + ChatColor.RESET + ChatColor.RED + " No Active Duels " + ChatColor.BOLD + "✘", ChatColor.WHITE.toString() + ChatColor.ITALIC + "You can start one yourself by typing:\n" + ChatColor.RESET + ChatColor.GREEN +  "/duel create");
-			ItemStack createDuel = Utility.createItem(Material.EMERALD_BLOCK, ChatColor.GREEN.toString() + ChatColor.BOLD +  "✔" + ChatColor.RESET + ChatColor.GREEN + " Click to " + ChatColor.UNDERLINE + "Create" + ChatColor.RESET + ChatColor.GREEN + " a Duel " + ChatColor.BOLD + "✔");
-			ItemStack duelNotifications = Utility.createItem(Material.REDSTONE, ChatColor.AQUA + "Duel Notifications: " + ChatColor.GREEN + ChatColor.BOLD + "✔", ChatColor.GREEN.toString() + ChatColor.BOLD + "✔" + ChatColor.RESET + ChatColor.GREEN + " Click to " + ChatColor.UNDERLINE + "Toggle" + ChatColor.RESET + ChatColor.GREEN + ChatColor.BOLD + " ✔");
-
-			// Maybe the inventory utility isn't as good as I thought, but I can learn from this.
-			Inventory inventory = Utility.createInventory(ChatColor.UNDERLINE.toString() + ChatColor.DARK_GRAY + "Active Duels", 45, Arrays.asList(Arrays.asList(frame, 4), Arrays.asList(duels, 1), Arrays.asList(frame, 5), Arrays.asList(null, 7), Arrays.asList(frame, 2), Arrays.asList(null, 3), Arrays.asList(duelStatus, 1), Arrays.asList(null, 3), Arrays.asList(frame, 2), Arrays.asList(null, 7), Arrays.asList(frame, 1), Arrays.asList(duelNotifications, 1), Arrays.asList(frame, 3), Arrays.asList(createDuel, 1), Arrays.asList(frame, 4)));
-
-			player.openInventory(inventory);
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 0) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			final ItemStack duels = Utility.createItem(Material.DIAMOND_SWORD, ChatColor.AQUA + "Duels",
+					(Multimap<Attribute, AttributeModifier>) null);
+
+			final ItemStack frame = Utility.createItem(Material.LIGHT_GRAY_STAINED_GLASS_PANE,
+					ChatColor.GRAY + "Complex Gaming");
+
+			final ItemStack duelStatus = Utility.createItem(Material.BARRIER,
+					ChatColor.RED.toString() + ChatColor.BOLD + "✘" + ChatColor.RESET + ChatColor.RED + " No Active Duels "
+							+ ChatColor.BOLD + "✘",
+					ChatColor.WHITE.toString() + ChatColor.ITALIC + "You can start one yourself by typing:\n" + ChatColor.RESET
+							+ ChatColor.GREEN + "/duel create");
+
+			final ItemStack createDuel = Utility.createItem(Material.EMERALD_BLOCK,
+					ChatColor.GREEN.toString() + ChatColor.BOLD + "✔" + ChatColor.RESET + ChatColor.GREEN + " Click to "
+							+ ChatColor.UNDERLINE + "Create" + ChatColor.RESET + ChatColor.GREEN + " a Duel " + ChatColor.BOLD + "✔");
+
+			final ItemStack duelNotifications = Utility.createItem(Material.REDSTONE,
+					ChatColor.AQUA + "Duel Notifications: " + ChatColor.GREEN + ChatColor.BOLD + "✔",
+					ChatColor.GREEN.toString() + ChatColor.BOLD + "✔" + ChatColor.RESET + ChatColor.GREEN + " Click to "
+							+ ChatColor.UNDERLINE + "Toggle" + ChatColor.RESET + ChatColor.GREEN + ChatColor.BOLD + " ✔");
+
+			// Maybe the inventory utility isn't as good as I thought, but I can learn from
+			// this.
+			final Inventory inventory = Utility.createInventory(
+					ChatColor.UNDERLINE.toString() + ChatColor.DARK_GRAY + "Active Duels", 45,
+
+					Arrays.asList(new Pair<ItemStack, Integer>(frame, 4), new Pair<ItemStack, Integer>(duels, 1),
+							new Pair<ItemStack, Integer>(frame, 5),
+							new Pair<ItemStack, Integer>(null, 7), new Pair<ItemStack, Integer>(frame, 2),
+							new Pair<ItemStack, Integer>(null, 3), new Pair<ItemStack, Integer>(duelStatus, 1),
+							new Pair<ItemStack, Integer>(null, 3), new Pair<ItemStack, Integer>(frame, 2),
+							new Pair<ItemStack, Integer>(null, 7), new Pair<ItemStack, Integer>(frame, 1),
+							new Pair<ItemStack, Integer>(duelNotifications, 1), new Pair<ItemStack, Integer>(frame, 3),
+							new Pair<ItemStack, Integer>(createDuel, 1),
+							new Pair<ItemStack, Integer>(frame, 4)));
+
+			player.openInventory(inventory);
+		} else {
+			Utility.log("/complex-gaming-menu is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
