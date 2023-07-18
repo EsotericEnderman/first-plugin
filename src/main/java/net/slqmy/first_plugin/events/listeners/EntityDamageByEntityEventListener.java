2017e956dbeca60cbf389465469cc24d0f4d8855
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-19 00:17:15 +0100

    🔫 Tweaked gun damage and fixed particle errors.

--- src/main/java/net/slqmy/first_plugin/events/listeners/EntityDamageByEntityEventListener.java
+++ src/main/java/net/slqmy/first_plugin/events/listeners/EntityDamageByEntityEventListener.java
@@ -1,9 +1,10 @@
 package net.slqmy.first_plugin.events.listeners;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
-import org.bukkit.Particle.DustOptions;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.World;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
@@ -17,18 +18,18 @@ import org.jetbrains.annotations.NotNull;
 import net.slqmy.first_plugin.FirstPlugin;
 
 public final class EntityDamageByEntityEventListener implements Listener {
+  private static final BlockData REDSTONE_BLOCK_DATA = Material.REDSTONE_BLOCK.createBlockData();
+
   private final NamespacedKey isShotgunBulletKey;
   private final NamespacedKey isPistolBulletKey;
   private final NamespacedKey isMiniGunBulletKey;
   private final NamespacedKey isGatlingGunBulletKey;
-  private final NamespacedKey isPoisonLauncherBulletKey;
 
   public EntityDamageByEntityEventListener(@NotNull final FirstPlugin plugin) {
     this.isPistolBulletKey = plugin.getIsPistolBulletKey();
     this.isShotgunBulletKey = plugin.getIsShotgunBulletKey();
     this.isMiniGunBulletKey = plugin.getIsMiniGunBulletKey();
     this.isGatlingGunBulletKey = plugin.getIsGatlingGunBulletKey();
-    this.isPoisonLauncherBulletKey = plugin.getIsPoisonLauncherBulletKey();
   }
 
   @EventHandler
@@ -48,19 +49,19 @@ public final class EntityDamageByEntityEventListener implements Listener {
       if (container.has(isPistolBulletKey, PersistentDataType.BOOLEAN)) {
 
         damageable.setHealth(Math.max(health - 10, 0));
-        world.spawnParticle(Particle.REDSTONE, location, 2, DustOptions.class);
+        world.spawnParticle(Particle.BLOCK_CRACK, location, 2, REDSTONE_BLOCK_DATA);
       } else if (container.has(isShotgunBulletKey, PersistentDataType.BOOLEAN)) {
 
-        damageable.setHealth(Math.max(health - 12, 0));
-        world.spawnParticle(Particle.REDSTONE, location, 5, DustOptions.class);
+        damageable.setHealth(Math.max(health - 24, 0));
+        world.spawnParticle(Particle.BLOCK_CRACK, location, 5, REDSTONE_BLOCK_DATA);
       } else if (container.has(isMiniGunBulletKey, PersistentDataType.BOOLEAN)) {
 
-        damageable.setHealth(Math.max(health - 8, 0));
-        world.spawnParticle(Particle.REDSTONE, location, 2, DustOptions.class);
+        damageable.setHealth(Math.max(health - 14, 0));
+        world.spawnParticle(Particle.BLOCK_CRACK, location, 2, REDSTONE_BLOCK_DATA);
       } else if (container.has(isGatlingGunBulletKey, PersistentDataType.BOOLEAN)) {
 
-        damageable.setHealth(Math.max(health - 9.5F, 0));
-        world.spawnParticle(Particle.REDSTONE, location, 6, DustOptions.class);
+        damageable.setHealth(Math.max(health - 14.5F, 0));
+        world.spawnParticle(Particle.BLOCK_CRACK, location, 6, REDSTONE_BLOCK_DATA);
       }
     }
   }
