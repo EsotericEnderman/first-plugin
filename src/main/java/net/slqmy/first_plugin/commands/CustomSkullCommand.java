b4658cb037e55ff81746e6ec515fcf80375f7afe
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:33:20 +0100

    💀 Made the `/custom-skull` command fully functional.

--- src/main/java/net/slqmy/first_plugin/commands/CustomSkullCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/CustomSkullCommand.java
@@ -1,7 +1,11 @@
 package net.slqmy.first_plugin.commands;
 
+import java.util.Arrays;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -10,27 +14,41 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
 
-public class CustomSkullCommand implements CommandExecutor {
+import net.slqmy.first_plugin.utility.Utility;
+
+public final class CustomSkullCommand implements CommandExecutor {
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
-
-			if (arguments.length == 0) {
-				player.sendMessage(ChatColor.RED + "Please provide one argument!");
-			} else {
-				ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
-
-				SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
-
-				// GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-
-				skullMeta.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + player.getName());
-
-				player.getInventory().addItem(skull);
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 1 || "".equals(Arrays.toString(args).trim())) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			final OfflinePlayer target = Bukkit.getOfflinePlayer(args[0]);
+
+			if (target.getName() == null) {
+				player.sendMessage(ChatColor.RED + "Player not found!");
+
+				return false;
+			}
+
+			final ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+			final SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+			skullMeta.setOwningPlayer(target);
+
+			skull.setItemMeta(skullMeta);
+
+			player.getInventory().addItem(skull);
+		} else {
+			Utility.log("/custom-skull is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
