3fc8b25365be4e807a2ce484e133fe1962d99c6d
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:41:41 +0100

    💬 Fixed messaging system + reply counts as message. (`/reply` will reply to a reply)

--- src/main/java/net/slqmy/first_plugin/commands/MessageCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/MessageCommand.java
@@ -8,51 +8,70 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import net.slqmy.first_plugin.FirstPlugin;
+import net.slqmy.first_plugin.utility.Utility;
+
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
-public class MessageCommand implements CommandExecutor {
+public final class MessageCommand implements CommandExecutor {
+	private static final int ARGUMENT_LENGTH = 2;
+
 	private final Map<UUID, UUID> recentMessages;
 
-	public MessageCommand(Map<UUID, UUID> recentMessages) {
-		this.recentMessages = recentMessages;
+	public MessageCommand(@NotNull final FirstPlugin plugin) {
+		this.recentMessages = plugin.getRecentMessages();
 	}
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
-
-			if (arguments.length < 2) {
-				player.sendMessage(ChatColor.RED + "Improper usage! Use /message <player> <message>.");
-			} else {
-				String target = arguments[0];
-
-				Player targetPlayer = Bukkit.getPlayerExact(target);
-				if (targetPlayer == null) {
-					player.sendMessage(ChatColor.RED + "Player not found.");
-				} else if (targetPlayer.getName().equals(player.getName())) {
-					player.sendMessage(ChatColor.RED + "You can't message yourself!");
-				} else {
-					StringBuilder message = new StringBuilder();
-
-					for (int i = 1; i < arguments.length; i++) {
-						message.append(arguments[i]).append(" ");
-					}
-
-					player.sendMessage(ChatColor.AQUA + "You » " + targetPlayer.getName() + ": " + ChatColor.WHITE + message);
-					targetPlayer.sendMessage(ChatColor.AQUA + player.getName() + " » You: " + ChatColor.WHITE + message);
-
-					UUID playerUUID = player.getUniqueId();
-					UUID targetPlayerUUID = targetPlayer.getUniqueId();
-
-					if (!recentMessages.containsKey(targetPlayerUUID) || recentMessages.get(targetPlayerUUID) != playerUUID ) {
-						recentMessages.put(targetPlayerUUID, playerUUID);
-					}
-				}
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length < ARGUMENT_LENGTH || "".equals(Arrays.toString(args).trim())) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			final String target = args[0];
+			final Player targetPlayer = Bukkit.getPlayerExact(target);
+
+			if (targetPlayer == null) {
+				player.sendMessage(ChatColor.RED + "Player not found.");
+				return true;
+			}
+
+			final String targetName = targetPlayer.getName();
+			final String playerName = player.getName();
+
+			if (targetName.equals(playerName)) {
+				player.sendMessage(ChatColor.RED + "You can't message yourself!");
+				return false;
+			}
+
+			final StringBuilder message = new StringBuilder();
+
+			for (int i = 1; i < args.length; i++) {
+				message.append(args[i]).append(" ");
+			}
+
+			player.sendMessage(ChatColor.AQUA + "You » " + targetName + ": " + ChatColor.WHITE + message);
+			targetPlayer.sendMessage(ChatColor.AQUA + playerName + " » You: " + ChatColor.WHITE + message);
+
+			final UUID playerUUID = player.getUniqueId();
+			final UUID targetPlayerUUID = targetPlayer.getUniqueId();
+
+			if (!recentMessages.containsKey(targetPlayerUUID) || recentMessages.get(targetPlayerUUID) != playerUUID) {
+				recentMessages.put(targetPlayerUUID, playerUUID);
+			}
+		} else {
+			Utility.log("/message is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
--- src/main/java/net/slqmy/first_plugin/commands/ReplyCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/ReplyCommand.java
@@ -8,49 +8,62 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import net.slqmy.first_plugin.FirstPlugin;
+import net.slqmy.first_plugin.utility.Utility;
+
+import java.util.Arrays;
 import java.util.Map;
 import java.util.UUID;
 
-public class ReplyCommand implements CommandExecutor {
+public final class ReplyCommand implements CommandExecutor {
 	private final Map<UUID, UUID> recentMessages;
 
-	public ReplyCommand(Map<UUID, UUID> recentMessages) {
-		this.recentMessages = recentMessages;
+	public ReplyCommand(@NotNull final FirstPlugin plugin) {
+		this.recentMessages = plugin.getRecentMessages();
 	}
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length == 0 || "".equals(Arrays.toString(args).trim())) {
+			return false;
+		}
 
-			if (arguments.length == 0) {
-				player.sendMessage(ChatColor.RED + "Please provide at least one argument!");
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			final UUID playerUUID = player.getUniqueId();
+			if (!recentMessages.containsKey(playerUUID)) {
+				player.sendMessage(ChatColor.RED + "No one has messaged you yet!");
 			} else {
-				UUID playerUUID = player.getUniqueId();
+				final UUID targetPlayerUUID = recentMessages.get(playerUUID);
 
-				if (!recentMessages.containsKey(playerUUID)) {
-					player.sendMessage(ChatColor.RED + "No one has messaged you yet!");
+				final Player targetPlayer = Bukkit.getPlayer(targetPlayerUUID);
+
+				if (targetPlayer == null) {
+					player.sendMessage(ChatColor.RED + "That player is not online!");
 				} else {
-					UUID targetUUID = recentMessages.get(playerUUID);
+					final StringBuilder message = new StringBuilder();
 
-					Player targetPlayer = Bukkit.getPlayer(targetUUID);
+					for (final String word : args) {
+						message.append(word).append(" ");
+					}
 
-					if (targetPlayer == null) {
-						player.sendMessage(ChatColor.RED + "That player is not online!");
-					} else {
-						StringBuilder message = new StringBuilder();
+					player.sendMessage(ChatColor.AQUA + "You » " + targetPlayer.getName() + ": " + ChatColor.WHITE + message);
+					targetPlayer.sendMessage(ChatColor.AQUA + player.getName() + " » You: " + ChatColor.WHITE + message);
 
-						for (String word : arguments) {
-							message.append(word).append(" ");
-						}
-
-						player.sendMessage(ChatColor.AQUA + "You » " + targetPlayer.getName() + ": " + ChatColor.WHITE + message);
-						targetPlayer.sendMessage(ChatColor.AQUA + player.getName() + " » You: " + ChatColor.WHITE + message);
+					if (!recentMessages.containsKey(targetPlayerUUID) || recentMessages.get(targetPlayerUUID) != playerUUID) {
+						recentMessages.put(targetPlayerUUID, playerUUID);
 					}
 				}
 			}
+		} else {
+			Utility.log("/reply is a player-only command!");
+
+			return false;
 		}
 
-		return false;
+		return true;
 	}
 }
