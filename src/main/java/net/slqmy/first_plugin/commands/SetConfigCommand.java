11f1fed4ffd91c25c1a7b7b258f0b8e36c61f283
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:42:00 +0100

    🌸 Improved code and aesthetics.

--- src/main/java/net/slqmy/first_plugin/commands/SetConfigCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/SetConfigCommand.java
@@ -1,39 +1,40 @@
 package net.slqmy.first_plugin.commands;
 
 import net.slqmy.first_plugin.FirstPlugin;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class SetConfigCommand implements CommandExecutor {
-	FirstPlugin firstPlugin;
+public final class SetConfigCommand implements CommandExecutor {
+	private static final int ARGUMENT_LENGTH = 2;
 
-	public SetConfigCommand(FirstPlugin firstPlugin) {
+	private final FirstPlugin firstPlugin;
+
+	public SetConfigCommand(@NotNull final FirstPlugin firstPlugin) {
 		this.firstPlugin = firstPlugin;
 	}
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player)commandSender;
-
-			if (arguments.length != 2) {
-				player.sendMessage("You must provide 2 arguments!");
-			} else {
-				firstPlugin.getConfig().set("Word", arguments[0]);
-				List<String> stringList = firstPlugin.getConfig().getStringList("Fruits");
-				stringList.add(arguments[1]);
-
-				firstPlugin.getConfig().set("Fruits", stringList);
-
-				firstPlugin.saveConfig();
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != ARGUMENT_LENGTH || "".equals(Arrays.toString(args).trim())) {
+			return false;
 		}
 
-		return false;
+		firstPlugin.getConfig().set("Word", args[0]);
+		final List<String> stringList = firstPlugin.getConfig().getStringList("Fruits");
+		stringList.add(args[1]);
+
+		firstPlugin.getConfig().set("Fruits", stringList);
+
+		firstPlugin.saveConfig();
+
+		return true;
 	}
 }
