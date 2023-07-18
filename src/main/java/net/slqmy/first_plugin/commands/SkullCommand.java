6fed30f62f0e79eb05253a30b383ecfc9b49b0ee
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:42:35 +0100

    💀 Removed the custom skull code as there is a command for it now.

--- src/main/java/net/slqmy/first_plugin/commands/SkullCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/SkullCommand.java
@@ -1,7 +1,5 @@
 package net.slqmy.first_plugin.commands;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -12,44 +10,33 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.lang.reflect.Field;
-import java.util.UUID;
+import net.slqmy.first_plugin.utility.Utility;
 
-public class SkullCommand implements CommandExecutor {
+public final class SkullCommand implements CommandExecutor {
 	@Override
-	public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String[] arguments) {
-		if (commandSender instanceof Player) {
-			Player player = (Player) commandSender;
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 0) {
+			return false;
+		}
 
-			ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
 
-			SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
+			final ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
+
+			final SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
 			assert skullMeta != null;
 			skullMeta.setOwningPlayer(player);
-			skullMeta.setDisplayName(ChatColor.GREEN.toString() + ChatColor.BOLD + player.getName());
 
 			skull.setItemMeta(skullMeta);
 
-			ItemStack itemStack = new ItemStack(Material.PLAYER_HEAD);
-			SkullMeta meta = (SkullMeta) itemStack.getItemMeta();
-
-			GameProfile profile = new GameProfile(UUID.randomUUID(), null);
-			profile.getProperties().put("textures", new Property("textures", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmEyYzNlNzlkNWYzNWE5ZGNhYjE5ZTQzYzNlM2E2NTE5ZTQyNmI2NGE2MTIxM2NkMmYxZDI4YjU3MDM2ZjYifX19"));
-
-			Field field;
-
-			try {
-				assert meta != null;
-				field = meta.getClass().getDeclaredField("profile");
-				field.setAccessible(true);
-
-				field.set(meta, profile);
-			} catch (NoSuchFieldException | IllegalAccessException e) {
-				e.printStackTrace();
-				return true;
-			}
-
 			player.getInventory().addItem(skull);
+		} else {
+			Utility.log("/skull is a player-only command!");
+
+			return false;
 		}
 
 		return true;
