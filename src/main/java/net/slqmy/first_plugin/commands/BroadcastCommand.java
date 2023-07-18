9ab9015d420c0ef225443ad9d429258322ab7982
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-18 22:06:08 +0100

    📣 Changed the `/broadcast` command to use an anvil GUI.

--- src/main/java/net/slqmy/first_plugin/commands/BroadcastCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/BroadcastCommand.java
@@ -1,45 +1,74 @@
 package net.slqmy.first_plugin.commands;
 
-import net.slqmy.first_plugin.events.ServerBroadcastEvent;
+import net.slqmy.first_plugin.FirstPlugin;
+import net.slqmy.first_plugin.events.custom_events.ServerBroadcastEvent;
+import net.slqmy.first_plugin.utility.Utility;
+import net.wesjd.anvilgui.AnvilGUI;
+import net.wesjd.anvilgui.AnvilGUI.ResponseAction;
+import net.wesjd.anvilgui.AnvilGUI.StateSnapshot;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BiFunction;
+
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-public class BroadcastCommand implements CommandExecutor {
+public final class BroadcastCommand implements CommandExecutor {
+	private final FirstPlugin plugin;
+
+	public BroadcastCommand(@NotNull final FirstPlugin plugin) {
+		this.plugin = plugin;
+	}
 
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-		if (sender instanceof Player) {
-			Player player = (Player) sender;
-
-			if (args.length == 0) {
-				player.sendMessage(ChatColor.RED + "You must provide a message to broadcast!");
-			} else {
-				StringBuilder builder = new StringBuilder();
-
-				for (String arg : args) {
-					builder.append(arg).append(" ");
-				}
-
-				String message = builder.toString();
-
-				ServerBroadcastEvent event = new ServerBroadcastEvent(player, message);
-				Bukkit.getPluginManager().callEvent(event);
-
-				if (!event.isCancelled()) {
-					Bukkit.broadcastMessage(message);
-
-					if (args[0].equalsIgnoreCase("hello")) {
-						player.sendMessage(ChatColor.AQUA + "Hello to you too!");
-					}
-				}
-			}
+	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
+			@NotNull final String label,
+			@NotNull final String[] args) {
+		if (args.length != 0) {
+			return false;
 		}
 
-		return false;
+		if (sender instanceof Player) {
+			final Player player = (Player) sender;
+
+			new AnvilGUI.Builder()
+					.title(ChatColor.BOLD + "Message")
+					.text("> Type here")
+					.itemLeft(new ItemStack(Material.WRITABLE_BOOK))
+					.onClick((final Integer integer, final StateSnapshot snapshot) -> {
+						final String message = snapshot.getText();
+
+						final ServerBroadcastEvent event = new ServerBroadcastEvent(player, message);
+						Bukkit.getPluginManager().callEvent(event);
+
+						if (!event.isCancelled()) {
+							Bukkit.broadcastMessage(ChatColor.GOLD + "Broadcast " + ChatColor.RESET + "» "
+									+ ChatColor.translateAlternateColorCodes('&', message));
+
+							if ("hello".equalsIgnoreCase(message)) {
+								player.sendMessage(ChatColor.DARK_BLUE.toString() + ChatColor.BOLD + "Hello to you too! "
+										+ ChatColor.RESET + ":)");
+							}
+						}
+
+						return Arrays.asList(ResponseAction.close());
+					})
+					.plugin(plugin)
+					.open(player);
+		} else {
+			Utility.log("/broadcast is a player-only command!");
+
+			return false;
+		}
+
+		return true;
 	}
 }
