9152c31e7e07c591a25022e37f55ec24ce38e244
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-19 11:31:46 +0100

    📄 Improved `/rizz` command. (though skin changing still isn't working)

--- src/main/java/net/slqmy/first_plugin/commands/RizzCommand.java
+++ src/main/java/net/slqmy/first_plugin/commands/RizzCommand.java
@@ -1,10 +1,14 @@
 package net.slqmy.first_plugin.commands;
 
+import com.xxmicloxx.NoteBlockAPI.model.Song;
+import com.xxmicloxx.NoteBlockAPI.songplayer.EntitySongPlayer;
 import com.xxmicloxx.NoteBlockAPI.songplayer.RadioSongPlayer;
 import com.xxmicloxx.NoteBlockAPI.utils.NBSDecoder;
 import net.slqmy.first_plugin.FirstPlugin;
 import net.slqmy.first_plugin.utility.Utility;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -12,31 +16,45 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.util.Arrays;
 
 public final class RizzCommand implements CommandExecutor {
-	private final FirstPlugin plugin;
+	private final Song whatIsLove;
 
 	public RizzCommand(final FirstPlugin plugin) {
-		this.plugin = plugin;
+		this.whatIsLove = NBSDecoder.parse(new File(plugin.getDataFolder(), "What is Love.nbs"));
 	}
 
 	@Override
 	public boolean onCommand(@NotNull final CommandSender sender, @NotNull final Command command,
 			@NotNull final String label,
 			@NotNull final String[] args) {
-		if (args.length != 0) {
+		if (args.length != 1 || "".equals(Arrays.toString(args).trim())) {
 			return false;
 		}
 
 		if (sender instanceof Player) {
-			Player player = (Player) sender;
+			final Player player = (Player) sender;
 
-			// Don't load files like this, use enums or classes that pre-parse the songs.
-			RadioSongPlayer songPlayer = new RadioSongPlayer(
-					NBSDecoder.parse(new File(plugin.getDataFolder(), "What is Love.nbs")));
+			final String targetName = args[0];
+			final Player target = Bukkit.getPlayer(targetName);
 
+			if (target == null) {
+				player.sendMessage(ChatColor.RED + "Player not found!");
+				return false;
+			}
+
+			final EntitySongPlayer songPlayer = new EntitySongPlayer(whatIsLove);
+
+			songPlayer.setEntity(player);
 			songPlayer.addPlayer(player);
+			songPlayer.addPlayer(target);
 			songPlayer.setPlaying(true);
+
+			Utility.setSkin(player,
+					"ewogICJ0aW1lc3RhbXAiIDogMTYxOTc3MjYyNzI0MiwKICAicHJvZmlsZUlkIiA6ICJjMGYzYjI3YTUwMDE0YzVhYjIxZDc5ZGRlMTAxZGZlMiIsCiAgInByb2ZpbGVOYW1lIiA6ICJDVUNGTDEzIiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzc3NDJjMDNjYjgwZGMxNGFmNTMzZjIwYzA1NDczNmZhMmMxNmU2YzIxNTFhMTE1MGUyOGVhOWYwNWM0MTcwNDQiCiAgICB9CiAgfQp9");
+			Utility.setSkin(target,
+					"ewogICJ0aW1lc3RhbXAiIDogMTYxOTcxNDczNzc3NywKICAicHJvZmlsZUlkIiA6ICJmNDY0NTcxNDNkMTU0ZmEwOTkxNjBlNGJmNzI3ZGNiOSIsCiAgInByb2ZpbGVOYW1lIiA6ICJSZWxhcGFnbzA1IiwKICAic2lnbmF0dXJlUmVxdWlyZWQiIDogdHJ1ZSwKICAidGV4dHVyZXMiIDogewogICAgIlNLSU4iIDogewogICAgICAidXJsIiA6ICJodHRwOi8vdGV4dHVyZXMubWluZWNyYWZ0Lm5ldC90ZXh0dXJlLzFkNmVjOGNiNTJjYWY2NzI1NWZmZGM2ZTEzYmE4M2M5NDNjMTQxMGIwNDVlOTJkOGIwYWM2MjAyMjM5ZTk3YjAiLAogICAgICAibWV0YWRhdGEiIDogewogICAgICAgICJtb2RlbCIgOiAic2xpbSIKICAgICAgfQogICAgfQogIH0KfQ==");
 
 			// songPlayer.destroy(); if I want to stop playing forever.
 			// songPlayer.setPlaying(false); to pause.
