122ae9d7fceda8ffbea952476b0f5ce87cc9d18e
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-07-21 12:59:23 +0100

    🐖 Attempted to improve the launch function...

--- src/main/java/net/slqmy/first_plugin/utility/VectorUtility.java
+++ src/main/java/net/slqmy/first_plugin/utility/VectorUtility.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Entity;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
 
+import net.slqmy.rank_system.utility.Utility;
+
 public final class VectorUtility {
 	private static final float GRAVITY = 32F;
 
@@ -18,6 +20,8 @@ public final class VectorUtility {
 
 		final double maxHeightGain = (rise > heightGainBlocks) ? rise + heightGainBlocks : heightGainBlocks;
 
+		Utility.log("Max Height Gain: " + maxHeightGain);
+
 		final double x_0 = origin.getX();
 		final double z_0 = origin.getZ();
 
@@ -27,12 +31,20 @@ public final class VectorUtility {
 		final double dx = x_n - x_0;
 		final double dz = z_n - z_0;
 
+		Utility.log("dx: " + dx);
+		Utility.log("dz: " + dz);
+
 		final double distance = Math.sqrt(dx * dx + dz * dz);
+
+		Utility.log("X and Z distance: " + distance);
 
 		final double x_3 = distance / 2;
 
 		final double alpha = Math.toDegrees(Math.atan2(maxHeightGain, x_3));
-		final double theta = alpha + 2F;
+		final double theta = Math.min(alpha + 2F, 89.99F);
+
+		Utility.log("Alpha: " + alpha);
+		Utility.log("Theta: " + theta);
 
 		final double thetaRadians = Math.toRadians(theta);
 
@@ -41,13 +53,23 @@ public final class VectorUtility {
 		final double magnitude = Math
 				.sqrt((-GRAVITY * x_3 * x_3) / (2 * cosine * cosine * (maxHeightGain - Math.tan(thetaRadians) * x_3)));
 
+		Utility.log("Magnitude: " + magnitude);
+
 		final double v_x_0 = Math.cos(thetaRadians) * magnitude;
 		final double v_y_0 = Math.sin(thetaRadians) * magnitude;
 
+		Utility.log("Initial horizontal velocity: " + v_x_0);
+		Utility.log("Initial vertical velocity: " + v_y_0);
+
 		final double beta = Math.atan2(dz, dx);
+
+		Utility.log("Beta: " + Math.toDegrees(beta));
 
 		final double x = Math.cos(beta) * v_x_0;
 		final double z = Math.sin(beta) * v_x_0;
+
+		Utility.log("X component: " + x);
+		Utility.log("Z component: " + z);
 
 		return new Vector(x / 20, v_y_0 / 20, z / 20);
 	}
